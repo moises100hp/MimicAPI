86481e04b1b4f9704e7925aed0ade32b37e3ae99
Moises Silva <moises100hp@gmail.com>
2021-07-08 21:28:19 -0300

    Implementado paginacao

--- MimicAPi/Controllers/PalavrasController.cs
+++ MimicAPi/Controllers/PalavrasController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MimicAPi.Database;
+using MimicAPi.Helpers;
 using MimicAPi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,9 +25,36 @@ namespace MimicAPi.Controllers
         //APP
         [Route("")]
         [HttpGet]
-        public ActionResult ObterPalavras()
+        public ActionResult ObterPalavras([FromQuery]PalavraUrlQuery query)
         {
-            return Ok(_banco.Palavras);
+            var item = _banco.Palavras.AsQueryable();
+
+            if (query.Data.HasValue)
+            {
+                item = item.Where(a => a.Criado > query.Data.Value || a.Atualizado > query.Data.Value);
+            }
+
+            if (query.PagNumero.HasValue)
+            {
+                //paginação
+                var quantidadeTotalRegistros = item.Count();
+                item = item.Skip((query.PagNumero.Value - 1) * query.PagRegistro.Value).Take(query.PagRegistro.Value);
+
+                var paginacao = new Paginacao();
+
+                paginacao.NumeroPagina = query.PagNumero.Value;
+                paginacao.RegistroPorPagina = query.PagRegistro.Value;
+                paginacao.TotalRegistros = quantidadeTotalRegistros;
+                paginacao.TotalPaginas = (int) Math.Ceiling((double) quantidadeTotalRegistros / query.PagRegistro.Value);
+
+                Response.Headers.Add("x-Pagination", JsonConvert.SerializeObject(paginacao));
+
+                if(query.PagNumero > paginacao.TotalPaginas)
+                {
+                    return NotFound();
+                }
+            }
+            return Ok(item);
         }
         //
         [Route("{id}")]
